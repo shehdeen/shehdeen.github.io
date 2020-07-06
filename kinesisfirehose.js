a9e59871748abe52c6f753f0b5ee4accdbab86dc
shehdeen <56844767+shehdeen@users.noreply.github.com>
2020-07-06 13:35:13 +1000

    Create kinesisfirehose.js

---
+++ kinesisfirehose.js
@@ -0,0 +1,26 @@
+// Configure Credentials to use Cognito
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'ap-southeast-2:447f978b-ff4b-48cb-9f52-9895c6918fe4'
+});
+
+AWS.config.region = 'ap-southeast-2';
+// We're going to partition Amazon Kinesis records based on an identity.
+// We need to get credentials first, then attach our event listeners.
+AWS.config.credentials.get(function(err) {
+
+PutRecordRequest putRecordRequest = new PutRecordRequest();
+putRecordRequest.setDeliveryStreamName(Shehdeen-test);
+
+String data = "testing";
+
+Record record = new Record().withData(ByteBuffer.wrap(data.getBytes()));
+putRecordRequest.setRecord(record);
+
+// Put record into the DeliveryStream
+firehoseClient.putRecord(putRecordRequest);   
+    
+    
+\
+    
+    
+});
